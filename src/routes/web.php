c53176c60dae394160a527b23ae8c217b0174cdf
Hillus Hilbert <hillus.souza@gmail.com>
2025-05-21 15:14:40 -0300

    criando template com blade usando design solicitado

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class,'index']);
